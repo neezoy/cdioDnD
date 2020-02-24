6cb1e83671ca0f9ec7b25afb96cce2d463db50f6
AngelofDeath666 <khejser@hotmail.com>
2020-02-24 20:01:08 +0100

    Made menu screen

--- src/com/cdioDnD/UI/TUI.java
+++ src/com/cdioDnD/UI/TUI.java
@@ -1,6 +1,5 @@
 package com.cdioDnD.UI;
 import com.cdioDnD.database.IDAO;
-import com.sun.org.apache.xerces.internal.impl.xs.models.XSCMUniOp;
 
 import java.util.Scanner;
 
@@ -17,8 +16,13 @@ public class TUI implements ITUI {
     public void showMenu() {
         scanner = new Scanner(System.in);
         System.out.println("Welcome to DND Forum");
-        System.out.println("1 Create A User");
-        System.out.println("2 Terminate Program");
+        System.out.println("1 Create");
+        System.out.println("2 Add");
+        System.out.println("3 Remove");
+        System.out.println("4 View");
+        System.out.println("5 Edit");
+        System.out.println("6 Delete");
+        System.out.println("7 Terminate Program");
 
         System.out.println("Please input your selection here: ");
         int menuItem = scanner.nextInt();
