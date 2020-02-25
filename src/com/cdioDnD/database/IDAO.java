e6ee5f0643beacb6b9bc1f0f96920cc3f6062546
Adin1128 <Adin11281128@hotmail.com>
2020-02-25 10:32:21 +0100

    Created some functions which overwrite or delete data, and fixed some earlier mistakes.

--- src/com/cdioDnD/database/IDAO.java
+++ src/com/cdioDnD/database/IDAO.java
@@ -1,37 +1,51 @@
 package com.cdioDnD.database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface IDAO {
     //First we define functions to create and get rows in our database 'Data' tables.
-    void createUser(String username, String Password, int role);
-    int getUserID(String username);
-    UserDTO getUser(int userid);
+    void createUser(UserDTO user, Connection c); //should this just be userDTO
+    int getUserID(String username, Connection c);
+    UserDTO getUser(int userid, Connection c);
 
-    void createCharacter(String charactername, int str, int n);
-    int getCharacterID(String charactername);
-    CharacterDTO getCharacter(int characterid);
+    void createCharacter(CharacterDTO character, Connection c);
+    int getCharacterID(String charactername, Connection c);
+    CharacterDTO getCharacter(int characterid, Connection c);
 
-    void createItem(String itemname, String description, double weight);
-    int getItemID(String itemname);
-    ItemDTO getItem(int itemid);
+    void createItem(ItemDTO item, Connection c);
+    int getItemID(String itemname, Connection c);
+    ItemDTO getItem(int itemid, Connection c);
 
-    void createGroup(String groupname, String description);
-    int getGroupID(String groupname);
-    GroupDTO getGroup(int groupid);
+    void createGroup(GroupDTO group, Connection c);
+    int getGroupID(String groupname, Connection c);
+    GroupDTO getGroup(int groupid, Connection c);
     
     //Now we define functions to create and delete rows in the relational tables.
-    void addCharacter(int userid, int characterid);
-    void removeCharacter(int characterid); //Only needs characterid because a character can only be owned by one person.
-    int[] getCharacterIDs(int userid);
+    void addCharacter(int userid, int characterid, Connection c);
+    void removeCharacter(int characterid, Connection c); //Only needs characterid because a character can only be owned by one person.
+    int[] getCharacterIDs(int userid, Connection c);
 
-    void addItem(int characterid, int itemid);
-    void addItems(int characterid, int itemid, int amount);
-    void removeItem(int characterid, int itemid);
-    void removeItems(int characterid, int itemid, int amount);
-    int[] getItemIDs(int characterid);
+    void addItem(int characterid, int itemid, Connection c);
+    void addItems(int characterid, int itemid, int amount, Connection c);
+    void removeItem(int characterid, int itemid, Connection c);
+    void removeItems(int characterid, int itemid, int amount, Connection c);
+    int[] getItemIDs(int characterid, Connection c);
 
-    void addToGroup(int characterid, int groupid);
-    void removeFromGroup(int characterid, int groupid);
-    int[] getGroupIDs(int characterid);
-    int[] getMembers(int groupid);
-    //TODO: Do we need delete functions and editing functions for each variable, or would this be done in a complete overwrite function which changes all the desired variables?
+    void addToGroup(int characterid, int groupid, Connection c);
+    void removeFromGroup(int characterid, int groupid, Connection c);
+    int[] getGroupIDs(int characterid, Connection c);
+    int[] getMembers(int groupid, Connection c);
+
+    //Lastly we define functions which deletes or overwrites data from the database.
+    void deleteUser(int userid, Connection c);
+    void overwriteUser(UserDTO user, Connection c);
+    void overwriteCharacter(CharacterDTO character, Connection c);
+    void overwriteItem(ItemDTO item, Connection c);
+    void deleteItem(int itemid, Connection c);
+    void overwriteGroup(GroupDTO group, Connection c);
+    void deleteGroup(int groupid, Connection c);
 }
