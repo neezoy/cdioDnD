0f6e96ad49db976fdc2321dec7816c0781f8915b
Adin1128 <Adin11281128@hotmail.com>
2020-02-25 08:35:32 +0100

    Created different required functions in the DAO Interface. Still missing some arguments and functions

--- src/com/cdioDnD/database/IDAO.java
+++ src/com/cdioDnD/database/IDAO.java
@@ -1,8 +1,42 @@
 package com.cdioDnD.database;
 
 public interface IDAO {
+    //First we define functions to create and get rows in our database 'Data' tables.
+    void createUser(String username, String Password, int role);
+    int getUserID();
+    UserDTO getUser();
 
+    void createCharacter(String name, int str, int n);
+    int getCharacterID();
+    CharacterDTO getCharacter();
+
+    void createItem();
+    int getItemID();
+    ItemDTO getItem();
+
+    void createGroup();
+    int getGroupID();
+    GroupDTO getGroup();
     
+    //Now we define functions to create and delete rows in the relational tables.
+    void addChar(int userid, int characterid);
+    void removeChar();
+    int[] getCharIDs();
 
+    void addItem();
+    void addItems();
+    void removeItem();
+    void removeItems();
+    int[] getItemIDs();
+
+    void addToGroup();
+    void removeFromGroup();
+    int[] getGroupIDs();
+    int[] getMembers();
+
+    //Lastly we define funtions which edit or delete data from the 'Data' tables.
+
+    void deleteUser(int userid);
+    void changeUsername(int userid, String username);
 
 }
