252acde9260c8d2ed65dbff1f7922b52db8ea01c
AngelofDeath666 <khejser@hotmail.com>
2020-02-26 22:28:17 +0100

    Cyka Blyat :D

--- src/com/cdioDnD/UI/TUI.java
+++ src/com/cdioDnD/UI/TUI.java
@@ -2,6 +2,7 @@ package com.cdioDnD.UI;
 import com.cdioDnD.database.IDAO;
 
 import javax.swing.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TUI implements ITUI {
@@ -16,21 +17,23 @@ public class TUI implements ITUI {
     @Override
     public void showMenu() {
         scanner = new Scanner(System.in);
-        System.out.println("Welcome to DND Forum");
-        System.out.println("1 Create");
-        System.out.println("2 Add");
-        System.out.println("3 Remove");
-        System.out.println("4 View");
-        System.out.println("5 Edit");
-        System.out.println("6 Delete");
-        System.out.println("7 Terminate Program");
 
-        System.out.println("Please input the number of your selection here: ");
         while (true) {
             try {
                 int menuItem = scanner.nextInt();
+                System.out.println("Welcome to DND Forum");
+                System.out.println("1 Create");
+                System.out.println("2 Add");
+                System.out.println("3 Remove");
+                System.out.println("4 View");
+                System.out.println("5 Edit");
+                System.out.println("6 Delete");
+                System.out.println("7 Terminate Program");
 
-            } catch (Exception e) {
+                System.out.println("Please input the number of your selection here: ");
+
+            } catch (InputMismatchException e) {
+
                 System.err.println("Please try again, input has to be a number");
 
             }
@@ -38,15 +41,29 @@ public class TUI implements ITUI {
             System.out.println("\n");
         }
 
-        switch (menuItem) {
-            case 1:
-
-        }
+//        switch (menuItem) {
+//            case 1:
+//
+//        }
     }
 
     @Override
-    public void createUser() {
-        System.out.println("Please ");
+    public void create() {
+        while (true) {
+            try {
+                int menuItem = scanner.nextInt();
+                System.out.println("You've chosen 'Create': ");
+                System.out.println("1 Create User");
+                System.out.println("2 Create Character");
+                System.out.println("3 Create Item");
+                System.out.println("4 Create Group");
+                System.out.println("Please input the number of your selection here: ");
+            } catch (InputMismatchException e) {
+                System.err.println("Please try again, input has to be a number");
+            }
+
+        }
+        System.out.println("\n");
     }
 
 }
