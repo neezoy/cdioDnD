06b6972dc62ad4f05df2e2c806af168befb29884
AngelofDeath666 <khejser@hotmail.com>
2020-03-01 16:02:27 +0100

    Created add and remove

--- src/com/cdioDnD/UI/TUI.java
+++ src/com/cdioDnD/UI/TUI.java
@@ -57,7 +57,6 @@ public class TUI implements ITUI {
     public void create() {
         while (true) {
             try {
-                menuItem = scanner.nextInt();
                 System.out.println("You've chosen 'Create': ");
                 System.out.println("1 Create User");
                 System.out.println("2 Create Character");
@@ -65,6 +64,7 @@ public class TUI implements ITUI {
                 System.out.println("4 Create Group");
                 System.out.println("5 Return To Previous Menu");
                 System.out.println("Please input the number of your selection here: ");
+                menuItem = scanner.nextInt();
                 System.out.println("\n");
 
             } catch (InputMismatchException e) {
@@ -79,11 +79,51 @@ public class TUI implements ITUI {
         }
     }
 
+    public void add () {
+        while (true) {
+            try {
+                System.out.println("You've Chosen 'Add': ");
+                System.out.println("2 Add Character To User");
+                System.out.println("3 Add Item To Character");
+                System.out.println("4 Add Group To Character\"");
+                System.out.println("5 Return To Previous Menu");
+                System.out.println("Please input the number of your selection here: ");
+                menuItem = scanner.nextInt();
+                System.out.println("\n");
 
+            } catch (InputMismatchException e) {
+                System.err.println("Please try again, input has to be a number");
+            }
 
+            switch (menuItem) {
 
+            }
+        }
 
+    }
 
+    public void remove () {
+        while (true) {
+            try {
+                System.out.println("You've Chosen 'Remove': ");
+                System.out.println("2 Remove Character To User");
+                System.out.println("3 Remove Item To Character");
+                System.out.println("4 Remove Group To Character\"");
+                System.out.println("5 Return To Previous Menu");
+                System.out.println("Please input the number of your selection here: ");
+                menuItem = scanner.nextInt();
+                System.out.println("\n");
+
+            } catch (InputMismatchException e) {
+                System.err.println("Please try again, input has to be a number");
+            }
+
+            switch (menuItem) {
+
+            }
+        }
+
+    }
 
 
 }
