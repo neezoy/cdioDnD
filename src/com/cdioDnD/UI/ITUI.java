8c7b0282bbf76226c14b10fc6acaba046129634d
AngelofDeath666 <khejser@hotmail.com>
2020-03-02 16:13:34 +0100

    Cyka blyat

--- src/com/cdioDnD/UI/ITUI.java
+++ src/com/cdioDnD/UI/ITUI.java
@@ -2,23 +2,7 @@ package com.cdioDnD.UI;
 
 
 public interface ITUI {
-    void showMenu();
-    void createMenu();
-    void addMenu ();
-    void removeMenu ();
-    void viewMenu();
-    void showCharacters();
-    void showGroups;
-    void showItems;
-    void showCharacter;
-    void showGroup;
-    void showItem;
-    void createCharater;
-    void createGroup;
-    void createItem;
-    void editCharacter;
-    void editGroup;
-    void editItem;
+    // se DTO interfaces
 
 
 }
