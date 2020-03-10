c9b924b85540bb92924210290209656cd0941f6d
AngelofDeath666 <khejser@hotmail.com>
2020-03-10 19:26:49 +0100

    ITUI should be done now

--- src/com/cdioDnD/UI/ITUI.java
+++ src/com/cdioDnD/UI/ITUI.java
@@ -1,8 +1,11 @@
 package com.cdioDnD.UI;
 
 
+import com.cdioDnD.dataTypes.*;
+
+import java.util.ArrayList;
+
 public interface ITUI {
-    // se DTO interfaces
 
     //menu
     int showMenu();
@@ -12,6 +15,41 @@ public interface ITUI {
     int viewMenu();
     int editMenu();
     int deleteMenu();
+
+    //create
+    void createUser(IUserDTO user); //don't know if this one is correct
+    void createCharacter(ICharacterDTO character);
+    void createItem(IItemDTO item);
+    void createGroup(IGroupDTO group);
+
+    //add
+    void addCharacter(ICharacterDTO character);
+    void addItem(IItemDTO item);
+    void addGroup(IGroupDTO group);
+
+    //remove
+    void removeCharacter(ICharacterDTO character);
+    void removeItem(IItemDTO item);
+    void removeGroup(IGroupDTO group);
+
+    //view
+    ArrayList<ICharacterDTO> getAllCharacters();
+    ArrayList<IGroupDTO> getAllGroups();
+    ArrayList<IItemDTO> getAllItems();
+    void viewOneCharacter(ICharacterDTO character);
+    void viewOneItem(IItemDTO item);
+    void viewOneGroup(IGroupDTO group);
+
+    //edit
+    void editUser(IUserDTO user); //don't know if this one is correct
+    void editCharacter(ICharacterDTO character);
+    void editItem(IItemDTO item);
+    void editGroup(IGroupDTO group);
+
+    //delete
+    void deleteUser(IUserDTO user); //or this :)
+    void deleteeItem(IItemDTO item);
+    void deleteGroup(IGroupDTO group);
 
 
 }
