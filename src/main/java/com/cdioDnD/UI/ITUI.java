6f919694e89f1887e08f3c2e89e7ffb55d428770
airo <valdemar_hh@hotmail.com>
2020-03-12 21:29:40 +0100

    State pattern implemented

--- src/main/java/com/cdioDnD/UI/ITUI.java
+++ src/main/java/com/cdioDnD/UI/ITUI.java
@@ -3,7 +3,6 @@ package com.cdioDnD.UI;
 
 import com.cdioDnD.dataTypes.*;
 
-import java.util.ArrayList;
 
 public interface ITUI {
 
@@ -16,42 +15,6 @@ public interface ITUI {
     int editMenu();
     int deleteMenu();
 
-    /* Dette er ikke implementeret
 
-    //create
-    UserDTO createUser(); //1 for the switch case
-    CharacterDTO createCharacter(); //2
-    ItemDTO createItem(); //3
-    GroupDTO createGroup(); //4
-
-    //add
-    CharacterDTO addCharacter(); //5
-    ItemDTO addItem();//6
-    GroupDTO addGroup(); //7
-
-    //remove
-    CharacterDTO removeCharacter(); //8
-    ItemDTO removeItem(); //9
-    GroupDTO removeGroup(); //10
-
-    //view
-    ArrayList<ICharacterDTO> getAllCharacters(); //11
-    ArrayList<IGroupDTO> getAllGroups(); //12
-    ArrayList<IItemDTO> getAllItems(); //13
-    CharacterDTO viewOneCharacter(); //14
-    ItemDTO viewOneItem(); //15
-    GroupDTO viewOneGroup(); //16
-
-    //edit
-    UserDTO editUser(); //17
-    CharacterDTO editCharacter(); //18
-    ItemDTO editItem(); //19
-    GroupDTO editGroup(); //20
-
-    //delete
-    UserDTO deleteUser(); //21
-    ItemDTO deleteeItem(); //22
-    GroupDTO deleteGroup(); //23
-*/
 
 }
--- src/main/java/com/cdioDnD/controller/MainController.java
+++
@@ -1,9 +0,0 @@
-package com.cdioDnD.controller;
-
-public class MainController {
-
-    public static void main(String[] args){
-
-
-    }
-}
