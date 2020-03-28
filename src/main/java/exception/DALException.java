32a7292ac16e1c70d91392fcf110963cb4242d07
Alan <s122417@student.dtu.dk>
2020-03-28 19:53:59 +0100

    DALException added.

---
+++ src/main/java/exception/DALException.java
@@ -0,0 +1,17 @@
+package exception;
+
+
+public class DALException extends Exception {
+    //private static final long serialVersionUID =
+
+    public DALException()
+    {
+        super();
+    }
+
+    public DALException(String msg)
+    {
+        super(msg);
+    }
+    
+}
